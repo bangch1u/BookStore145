cfc5e60947338f0702fc2325b39cfff6189328c7
NguyenTrongBang <bang09688@gmail.com>
2024-12-07 08:49:53 +0700

    create form add bookEntry

--- BookBlazorWasmCustomer/Program.cs
+++ BookBlazorWasmCustomer/Program.cs
@@ -3,11 +3,14 @@ using BookBlazorWasmCustomer.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using static BookBlazorWasmCustomer.Pages.MainPages.BooksPage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7177") });
-builder.Services.AddScoped<IBookApiClient, BookApiClient>();    
+builder.Services.AddScoped<IBookApiClient, BookApiClient>();
+builder.Services.AddSingleton<BookEntryService>();
+
 await builder.Build().RunAsync();
---
+++ BookBlazorWasmCustomer/Services/BookEntryService.cs
@@ -0,0 +1,30 @@
+﻿namespace BookBlazorWasmCustomer.Services
+{
+    public class BookEntryService
+    {
+      
+       
+        public List<BookEntry> BookEntrys { get; private set; } = new List<BookEntry>();
+
+        public void AddBookEntry(Guid bookId)
+        {
+           var bookEntryNew = new BookEntry();
+           bookEntryNew.BookEntryId = bookId;
+            BookEntrys.Add(bookEntryNew);   
+
+
+        }
+
+    }
+    public class BookEntry
+    {
+        public Guid BookEntryId { get; set; }
+        public int Quantity { get; set; }
+
+        public BookEntry()
+        {
+              Quantity = 0;
+        }
+    }
+
+}
